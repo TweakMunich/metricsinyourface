38eddac0264ea4310514482cec8a9700eda91a84
EC2 Default User <ec2-user@ip-172-31-20-81.us-west-2.compute.internal>
2015-10-08 14:37:12 -0700

    present simple HTML form on '/' path. Allow post of two variables. Error handling

--- server/app.js
+++ server/app.js
@@ -8,17 +8,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var array = [];
 
 app.post('/setValue', function (req, res) {
-    var data = req.body;
-    array[data.id] = data.value;
-    res.send(data);
+    var id = req.body.id;
+    var value = req.body.value;
+    if (id && value) {
+      array[id] = value;
+      res.send(id + ': ' + value);
+    } else {
+      res.status(400).send('must post "id" and "value"');
+    }
 });
 
 app.get('/getValue', function (req, res) {
-    if(req.param("id"))
-    {
+    if(req.param("id")) {
         res.send({"id" : req.param("id"),
                 "value" : array[req.param("id")]});
     }
+});
+
+app.get('/', function (req, res) {
+    res.send('<html><form action="/setValue" method="post">' + 
+            'id: ' + 
+            '<input type="text" name="id"><br>' +
+            'value: ' + 
+            '<input type="text" name="value"><br>' +
+            '<input type="submit" value="Submit">' +
+            '<form/></html>');
 });
 
 var server = app.listen(port, function () {

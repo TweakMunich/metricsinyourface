be0eb4241bea9cf9b267e37899d4165c586bf9a7
Gregor <ghohpe@gmail.com>
2015-12-25 00:14:20 +0100

    fake config reader with hard coded values to testing or use with i2c displays

---
+++ client/readconfig_fake.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+#
+# Returns a fake config consisting of 2 4 digits displays
+# pegged to ID 5 and 6. Import this instead of readconfig.py for testing.
+
+def setup():
+  return None
+
+def read_config():
+  """ returns a list of tupels indicating (num digits, ID) for each
+      connected display. Hard coded to 4-digit displays pegged to 
+      ID 5 and 6. Useful when using i2c displays without any serial."""
+  return [(4, 5), (4, 6)]
+

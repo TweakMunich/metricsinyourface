41995788927b1a0508fcd900fc8c8861e7b7755e
SpaceLord <ghohpe@gmail.com>
2016-01-09 09:53:01 +0000

    Ignore non-printable characters. Allow degree symbol for temp displays.

--- client/sevenseg.py
+++ client/sevenseg.py
@@ -5,11 +5,12 @@
 # Deals with max display size, suppressing leading zeroes etc. 
 
 # 7 Segment coding a=bit0, b=bit1, ..., DP=bit7
+# Lowercase o can be used as "Degree" symbol
 
 SEGMENTS = [0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110,
             0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111]
 SPECIAL = { 'u': 0b00011100, 'c': 0b01011000 , '_': 0b00001000 ,
-            '-': 0b01000000, ' ': 0b00000000 }
+            '-': 0b01000000, ' ': 0b00000000 , 'o': 0b01100011}
 
 def blank():
   """ Returns 7-segment code for a blank. """
@@ -26,12 +27,23 @@ def char(value, dp=False):
   elif value in SPECIAL:
     return (SPECIAL[value] | (0x80 * dp))
 
+def charList(value, dp=False):
+  """ Returns a list containing the raw code if there is one,
+      or an empty list if not. """
+  raw = char(value,dp)
+  if raw is None:
+    return []
+  else:
+    return [raw]
+
 def text(value, digits):
   """ Returns an array of 7-segment codings corresponding to a string or an
       integer number.
 
       If the string is shorter than the specified digits, it is padded on 
       the left. If it is longer, only the rightmost characters are included.
+
+      Nonprintable characters are omitted.
 
       Periods are not separate characters but add a decimal point to the
       preceding character. To just print a period, specify " .".  A decimal
@@ -41,14 +53,14 @@ def text(value, digits):
   c1 = None
   for c in value:
     if c == '.':
-      result += [char(c1 if c1 else ' ', True)]
+      result += charList(c1 if c1 else ' ', True)
       c1 = None
     else:
       if c1:
-        result += [char(c1)]
+        result += charList(c1)
       c1 = c
   if c1:
-    result += [char(c1)]
+    result += charList(c1)
   result = result[-digits:]
   while len(result) < digits:
     result = [blank()] + result
@@ -70,6 +82,7 @@ def main():
   print text('21111', 4)
   print text(111, 4)
   print text(11111, 4)
+  print text('xx1', 4)
 
 if __name__ == "__main__":
   main()

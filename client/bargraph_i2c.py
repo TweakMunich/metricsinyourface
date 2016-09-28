89481b07c1c801d86e574b54bcfd8b48fba85bf9
yuanchen <yx@allianz.com>
2016-09-28 14:33:21 +0000

    handle strings, error check

--- client/bargraph_i2c.py
+++ client/bargraph_i2c.py
@@ -16,9 +16,9 @@ from Adafruit_LED_Backpack import BicolorBargraph24
 import sys
 import time
 
-class BarGraph:
+class BarGraphDisplay:
 
-  def __init__(self, yellow=70, red=85, address=None):
+  def __init__(self, yellow=50, red=75, address=None):
     """ Sets up one physical display. yellow and red define percentage of value 
         where color changes. """
     self.bar = BicolorBargraph24.BicolorBargraph24(address = address)
@@ -31,13 +31,29 @@ class BarGraph:
   def cleanup(self):
     return None
  
+  def start(self):
+    self.bar.clear()
+ 
+  def latch(self):
+    self.bar.write_display()
+ 
   def calcbar(self, value):
     """ Converts from 0 to 100 range into bar index, i.e. 0 - 23 """
     return (value + 4) * 24 / 100
 
   def output(self, value):
     """ Outputs an integer between 0 and 99 to bar graph display."""
-    self.bar.clear()
+    try:
+      value = int(float(value.strip('.')))
+    except ValueError:
+      value = 0
+
+    if value < 0:
+      value = 0
+    if value > 99:
+      value = 99
+    
+    self.start()
     for num in range(0, self.calcbar(value)):
       if num >= self.calcbar(self.red):
         color = BicolorBargraph24.RED
@@ -46,7 +62,7 @@ class BarGraph:
       else:
         color = BicolorBargraph24.GREEN
       self.bar.set_bar(num, color)
-    self.bar.write_display()
+    self.latch()
 
   def blank(self):
     self.bar.clear()
@@ -60,19 +76,21 @@ def main():
 
   if len(args) < 2:
     # count on the first display
-    display = BarGraph(50, 75, address = 0x70)
+    display = BarGraphDisplay(50, 75, address = 0x71)
     display.setup()
-    display.blank()
     for num in range(0,101):
+      display.start()
       display.output(num)
+      display.latch()
       time.sleep(0.05)
     display.cleanup()
   else:
     value = int(args[1])
-    display = BarGraph(address = 0x70)
+    display = BarGraphDisplay(address = 0x71)
     display.setup()
-    display.blank()
+    display.start()
     display.output(value)
+    display.latch()
 
 if __name__ == "__main__":
   main()
